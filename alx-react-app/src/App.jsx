9932551ca6d1f47903d1c7a3d16cb91f8f1a3266
Ayoku Ibrahim <ayokuadewalex@gmail.com>
2025-02-18 22:01:46 +0100

    create new folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-    
+
 }
 
 export default App
