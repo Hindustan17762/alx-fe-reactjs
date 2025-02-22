3f32879a0a2e54a267dd759e331f2cdc666cf4b8
Ayoku Ibrahim <ayokuadewalex@gmail.com>
2025-02-22 20:32:08 +0100

    create the project of my-company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', textAlign: 'center' }}>
+        <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
